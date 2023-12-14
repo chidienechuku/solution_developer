ee78c5e93725fc5436626d65498b333dc0a5c5a6
Chidi Enechuku <106663046+chidienechuku@users.noreply.github.com>
2023-12-14 13:05:46 +0200

    Add files via upload

---
+++ accessdenied.php
@@ -0,0 +1,42 @@
+<?php 
+
+
+	session_destroy();
+	echo "
+			<html>
+				<head>
+					<title>
+						WFP South Sudan Intranet Logout
+					</title>
+				</head>
+				<body>
+					<table border=0 width=\"100%\" id=table2 bgcolor= #1f90ff>
+	        			<tr>
+ 			  				<td width=110>
+								<img border=0 src=img/WFP_logo_white.gif >
+							</td>
+						</tr>
+		  			</table>
+		  			<table border=0 width=100% heigth=100% background=img/untitled2.jpg>
+	       				<tr>
+			  				<td height=400 align=center>
+			    				<b>
+								<font size=4 color=\"#FFFFFF\" face=\"Calibri\">
+								Access Denied. Kindly contact Juba IT ServiceDesk at juba.itservicedesk@wfp.org<br><br>
+								</font></b>
+				 				<a href='ssd.wfp.org/auth'/>
+				 				<font size=4 color=\"#FFFFFF\" face=\"Calibri\">
+								Click Here
+								</font></a>
+								<font size=4 color=\"#FFFFFF\" face=\"Calibri\">
+								if you want to go back to the home page.
+				 				<br><br>Thank you.
+								</font></b>
+			  				</td>
+						</tr>
+		  			</table>
+				</body>
+			</html>";
+  
+  
+?>
